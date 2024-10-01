0a6666513f705e67c46df1482111c9eacffd2a5c
Thiago Irênio <thiagoirenio@gmail.com>
2024-09-30 22:23:33 -0300

    oi

---
+++ javascriptTooling/proj01/app.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Define a pasta onde seus arquivos estáticos estão localizados
+app.use(express.static(path.join(__dirname)));
+
+// Rota raiz, redireciona para o arquivo index.html na pasta public
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
---
+++ javascriptTooling/proj01/main.js
@@ -0,0 +1,4 @@
+const nome = 'Thiago';
+const obj = {nome};
+const novoObj = { ...obj };
+console.log(novoObj);
