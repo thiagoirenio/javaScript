a50bb1b6cd2fb99984e53ab98cb48f3ada3b6f1e
Thiago Irênio <thiagoirenio@gmail.com>
2024-10-09 21:52:25 -0300

    oi

--- nodejs/aula04/index.js
+++ nodejs/aula04/index.js
@@ -1,2 +1,6 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
+
+fs.readdir('./')
+  .then(files => console.log(files))
+  .catch(e => console.log(e));
