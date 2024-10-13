2a47c4f63a783bf69896e1fac6b5f9d41b23aeb4
Thiago Irênio <thiagoirenio@gmail.com>
2024-10-13 17:45:19 -0300

    comitando

--- nodejs/aula13/routes.js
+++ nodejs/aula13/routes.js
@@ -4,6 +4,7 @@ const homeController = require('./src/controllers/homeController');
 const contatoController = require('./src/controllers/contatoController');
 
 function meuMiddleware(req, res, next) {
+  req.session = { nome: 'Thiago', sorenome: 'Irênio' };
   console.log();
   console.log('Passei no seu middleware.');
   console.log();
@@ -14,6 +15,7 @@ function meuMiddleware(req, res, next) {
 route.get('/', meuMiddleware, homeController.paginaInicial, function (req, res, next) {
   console.log();
   console.log('Ainda estou aqui...');
+  console.log(`Olha o que tem na req.session.nome ${req.session.nome}`);
 });
 route.post('/', homeController.trataPost);
 
--- nodejs/aula13/src/controllers/homeController.js
+++ nodejs/aula13/src/controllers/homeController.js
@@ -1,6 +1,7 @@
 exports.paginaInicial = (req, res, next) => {
   console.log('Respondendo ao cliente.');
   res.render('index');
+  console.log(`Olha o que tem na req.session.nome ${req.session.nome}`);
   next();
 };
 
