385cb67c409dbe872ee1c5d7c18e10b8c5a869bb
Thiago Irênio <thiagoirenio@gmail.com>
2024-10-22 19:34:22 -0300

    comitando isso

--- nodejs/aula17/server.js
+++ nodejs/aula17/server.js
@@ -19,7 +19,9 @@ const csrf = require('csurf');
 const { middlewareGlobal, checkCsrfError, csrfMiddleware } = require('./src/middlewares/middlewares');
 
 app.use(helmet());
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express, json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 const sessionOptions = session({
